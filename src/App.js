ed73154e2f84dac4b449d7bc0d466890a1559e96
Jose <jloai009@fiu.edu>
2022-02-10 03:10:50 -0500

    Implement useResource hook

--- src/App.js
+++ src/App.js
@@ -20,10 +20,27 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(() => {
+    axios
+      .get(baseUrl)
+      .then((response) => {
+        setResources(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [baseUrl])
+
 
   const create = (resource) => {
-    // ...
+    axios
+      .post(baseUrl, resource)
+      .then((response) => {
+        setResources(resources.concat(response.data))
+      })
+      .catch((error) => {
+        console.log(error)
+      })
   }
 
   const service = {
